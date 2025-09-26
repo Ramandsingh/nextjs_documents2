58a3421a4453d4a98dbba358a52aa46a8bac1129
Raman Singh <ramandsingh@gmail.com>
2025-09-26 10:32:36 +1000

    fix: update type for options parameter in cookies set function

--- lib/cookies.d.ts
+++ lib/cookies.d.ts
@@ -1,9 +1,7 @@
-import { cookies } from 'next/headers';
-
 declare module 'next/headers' {
   export function cookies(): {
     get: (name: string) => { name: string; value: string } | undefined;
-    set: (name: string, value: string, options?: any) => void;
+    set: (name: string, value: string, options?: object) => void;
     delete: (name: string) => void;
   };
 }
