f4735496618667e6912b7ed8ca740b1e742b9f67
Raman Singh <husseinsrour@me.com>
2025-09-27 15:55:23 +1000

    27091555

--- components/receipt-table.tsx
+++ components/receipt-table.tsx
@@ -1291,7 +1291,8 @@ export function ReceiptTable({ data }: ReceiptTableProps) {
 
           {/* Receipt Cards Grid */}
           <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
-            {data.map((receipt, index) => { // eslint-disable-line @typescript-eslint/no-unused-vars
+            {data.map((receipt, idx) => {
+              const index = idx;
               // Check if this receipt is flagged as duplicate
               const isDuplicate = duplicates.some(dup => dup.indices.includes(index));
 
